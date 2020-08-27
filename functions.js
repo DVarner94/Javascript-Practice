55038f344c3c61a23becefa9b507f63249088a53
Dominic Varner <dommy@MLBs-Lappy.local>
2020-08-27 12:42:02 -0700

    return practice

--- functions.js
+++ functions.js
@@ -1,18 +1,25 @@
-function rollDie(){
-    let roll = Math.floor(Math.random() * 6) + 1;
-    console.log(`Rolled: ${roll}`);
-}
+// function rollDie(){
+//     let roll = Math.floor(Math.random() * 6) + 1;
+//     console.log(`Rolled: ${roll}`);
+// }
 
-function throwDice(numRolls){
-    for (let i = 0; i < numRolls; i++){
-        rollDie();
-    }
-}
+// function throwDice(numRolls){
+//     for (let i = 0; i < numRolls; i++){
+//         rollDie();
+//     }
+// }
 
-function greet(name){
-    console.log(`Hi, ${name}`);
-}
+// function greet(name){
+//     console.log(`Hi, ${name}`);
+// }
 
-function add(x,y) {
-    return x + y;
-}
+// function add(x,y) {
+//     return x + y;
+// }
+
+// function square (x){
+//     s = return x * x;
+//     console.log('All done!');
+// }
+// ^console.log won't be executed because it's after a return statement
+
